a9c39c3b6acdbce7cc765b04df85fb495facf9e7
luchen825 <lucychen28@gmail.com>
2014-10-16 10:19:21 -0700

    Added Starfield

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] one;
-void setup()
+public void setup()
 {
 	size(600,600);
 	one = new Particle[101];
@@ -10,7 +26,7 @@ void setup()
 	//one[99] = new Jumbo();
 	one[100] = new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	fill(0,30);
 	rectMode(LEFT);
@@ -22,7 +38,7 @@ void draw()
 	}
 	
 }
-void mousePressed()
+public void mousePressed()
 {
 	for(int j=0; j < 100; j++)
 	{
@@ -49,7 +65,7 @@ class NormalParticle implements Particle
 	{
 		xNPos = xNPos + (Math.cos(angle))*speed;
 		yNPos = yNPos + (Math.sin(angle))*speed;
-		angle = angle + 0.01;
+		angle = angle + 0.01f;
 	}
 	public void show()
 	{
@@ -118,3 +134,12 @@ class OddballParticle implements Particle
 // }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
